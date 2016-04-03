824347159ec73c379ee9b7c6b8c76f562175062e
VergeA <andrew.verge64@gmail.com>
2016-04-03 01:47:50 -0400

    Print debugging to figure out how followers dictionary works

--- app.py
+++ app.py
@@ -34,9 +34,13 @@ def auth():
 @app.route('/getArtists', methods=['GET'])
 def getArtists(spot):
     followed_artists = spot.current_user_followed_artists(limit=30)
-    print followed_artists
+    artistNames(followed_artists)
     return render_template('concerts.html', data=map(json.dumps, followed_artists))
 
+def artistNames(artists):
+    names = []
+    for item in artists["items"]:
+        print item["name"]
 
 if __name__ == '__main__':
     app.debug = True
